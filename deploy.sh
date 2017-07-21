b36480a3d8ffe3c918b4e818b0895b9d76198b76
Micah Godbolt <micahgodbolt@gmail.com>
2017-07-21 09:26:42 -0700

    Basename

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 
 for f in stuff/*; do
-	echo "Uploading $f to $FTP_USER:$FTP_PASSWORD"
-	curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASSWORD ftp://waws-prod-bay-049.ftp.azurewebsites.windows.net/site/wwwroot/mgodbolt/vrt/$TRAVIS_PULL_REQUEST/$f
+    b=$(basename $f)
+	echo "Uploading $f as $b"
+	curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASSWORD ftp://waws-prod-bay-049.ftp.azurewebsites.windows.net/site/wwwroot/mgodbolt/vrt/$TRAVIS_PULL_REQUEST/$b
 done
